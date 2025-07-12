d3b8af046776e3287b9af98234d725f8ad5dd204
Juan Gomez <j.david116@utexas.edu>
2025-07-12 09:52:29 -0500

    Error Catching During Memory Retrieval

--- src/services/memory_service/memory.py
+++ src/services/memory_service/memory.py
@@ -13,11 +13,14 @@ class MemoryService:
     def __init__(self):
         self.memory_client = MemoryClient(api_key=os.getenv("MEM0AI_API_KEY"))
 
-    def retreive_conversation_history(self,agent: ConversableAgent, messages: list[dict[str, Any]]) -> None:        
-        relevant_memories = self.memory_client.search(messages[len(messages) - 1]["content"], user_id="user")
-        flatten_relevant_memories = "\n".join([m["memory"] for m in relevant_memories])
+    def retreive_conversation_history(self,agent: ConversableAgent, messages: list[dict[str, Any]]) -> None:  
+        try:
+            relevant_memories = self.memory_client.search(messages[len(messages) - 1]["content"], user_id="user")
+            flatten_relevant_memories = "\n".join([m["memory"] for m in relevant_memories])
 
-        agent.update_system_message(agent.system_message.format(context=flatten_relevant_memories))
+            agent.update_system_message(agent.system_message.format(context=flatten_relevant_memories))
+        except Exception as e:
+            print(f"Error retreiving conversation history: {e}")
 
     def log_conversation_to_mem0(self, message: Union[str, list[dict[str, Any]]]) -> str:
         if isinstance(message, list):
